8134eca2f68dda211c08405409374f9cff593fae
Jacob Maier <jacobamaier@gmail.com>
2023-02-19 14:39:47 -0800

    Updating Homework.

---
+++ ShapeTracker/Models/Triangle.cs
@@ -0,0 +1,36 @@
+namespace ShapeTracker.Models
+{
+  public class Triangle
+  {
+    public int Side1;
+    public int Side2 = 4;
+    public int Side3;
+
+    public Triangle(int length1, int length2, int length3)
+    {
+      Side1 = length1;
+      Side2 = length2;
+      Side3 = length3;
+    }
+
+    public string CheckType()
+    {
+      if ((Side1 > (Side2 + Side3)) || (Side2 > (Side1 + Side3)) || (Side3 > (Side1 + Side2)))
+      {
+        return "Not a triangle";
+      }
+      else if ((Side1 != Side2) && (Side2 != Side3) && (Side1 != Side3))
+      {
+        return "Scalene triangle";
+      }
+      else if ((Side1 == Side2) && (Side2 == Side3))
+      {
+        return "Equilateral triangle";
+      }
+      else
+      {
+        return "Iscosceles triangle";
+      }
+    }
+  }
+}
