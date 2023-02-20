92b8d47b4b851bdd5beee789c8a33f405f8a140f
Jacob Maier <jacobamaier@gmail.com>
2023-02-19 18:48:45 -0800

    updating with homework

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -1,41 +1,30 @@
+using System.Collections.Generic;
+
 namespace ShapeTracker.Models 
 {
   public class Triangle 
   {
     private int _side1;
-    private int _side2;
+    public int Side1
+    {
+      get { return _side1; }
+      set { _side1 = value; }
+    }
+    public int Side2 { get; set; }
     private int _side3;
+    private static List<Triangle> _instances = new List<Triangle> {};
 
     public Triangle(int length1, int length2, int length3) 
     {
       _side1 = length1;
-      _side2 = length2;
+      Side2 = length2;
       _side3 = length3;
-    }
-
-    public int GetSide1()
-    {
-      return _side1;
-    }
-
-    public int GetSide2()
-    {
-      return _side2;
+      _instances.Add(this);
     }
 
     public int GetSide3()
     {
       return _side3;
-    }
-
-    public void SetSide1(int newSide)
-    {
-      _side1 = newSide;
-    }
-
-    public void SetSide2(int newSide)
-    {
-      _side2 = newSide;
     }
 
     public void SetSide3(int newSide)
@@ -45,15 +34,15 @@ namespace ShapeTracker.Models
 
     public string CheckType() 
     {
-      if ((_side1 > (_side2 + _side3)) || (_side2 > (_side1 + _side3)) || (_side3 > (_side1 + _side2)))
+      if ((_side1 > (Side2 + _side3)) || (Side2 > (_side1 + _side3)) || (_side3 > (_side1 + Side2)))
       {
         return "not a triangle";
       } 
-      else if ((_side1 != _side2) && ((_side1 != _side3)) && ((_side2 != _side3))) 
+      else if ((_side1 != Side2) && ((_side1 != _side3)) && ((Side2 != _side3))) 
       {
         return "scalene triangle";
       }  
-      else if ((_side1 == _side2) && (_side1 == _side3)) 
+      else if ((_side1 == Side2) && (_side1 == _side3)) 
       {
         return "equilateral triangle";
       } 
@@ -61,6 +50,14 @@ namespace ShapeTracker.Models
       {
         return "isosceles triangle";
       }
+    }
+    public static List<Triangle> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -7,19 +7,68 @@ namespace ShapeTracker
   {
     static void Main()
     {
-      Triangle testTriangle = new Triangle(3,4,5);
-      Console.WriteLine($"Side one of the triangle: {testTriangle.GetSide1()}");
-      Console.WriteLine($"Side two of the triangle: {testTriangle.GetSide2()}");
-      Console.WriteLine($"Side three of the triangle: {testTriangle.GetSide3()}");
-      Console.WriteLine("Want to know what type of triangle you have?");
-      Console.WriteLine("Calculating...");
-      Console.WriteLine($"Your triangle is: {testTriangle.CheckType()}.");
-      testTriangle.SetSide1(44);
-      testTriangle.SetSide2(44);
-      testTriangle.SetSide3(70);
-      Console.WriteLine($"Actually, I've just changed the values of your triangle to {testTriangle.GetSide1()}. {testTriangle.GetSide2()}, {testTriangle.GetSide3()}");
-      Console.WriteLine($"Now your triangle is {testTriangle.CheckType()}.");
+      Console.WriteLine("----------------------");
+      Console.WriteLine("Welcome to Shape Tracker!");
+      Console.WriteLine("We'll calculate what type of shape you have based off of the lengths provided.");
+      Console.WriteLine("Please enter a number:");
+      string stringNumber1 = Console.ReadLine();  
+      Console.WriteLine("Enter another number:");
+      string stringNumber2 = Console.ReadLine();  
+      Console.WriteLine("Enter a third number:");
+      string stringNumber3 = Console.ReadLine();  
+      int length1 = int.Parse(stringNumber1);  
+      int length2 = int.Parse(stringNumber2);  
+      int length3 = int.Parse(stringNumber3);  
+      Triangle tri = new Triangle(length1, length2, length3);
+      ConfirmOrEditTriangle(tri);
+    }
 
+    static void ConfirmOrEditTriangle(Triangle tri)
+    {
+      Console.WriteLine("Please confirm that you entered in your shape correctly:");
+      Console.WriteLine($"Side 1 has a length of {tri.Side1}.");
+      Console.WriteLine($"Side 2 has a length of {tri.Side2}.");
+      Console.WriteLine($"Side 3 has a length of {tri.GetSide3()}.");
+      Console.WriteLine("Is that correct? Enter 'yes' to proceed, or 'no' to re-enter the shapes sides");
+      string userInput = Console.ReadLine();  
+      if (userInput == "yes")
+      {
+        CheckTriangleType(tri);
+      }
+      else
+      {
+        Console.WriteLine("Let's fix your triangle. Please enter the sides again!");
+        Console.WriteLine("Please enter a number:");
+        string stringNumber1 = Console.ReadLine();  
+        Console.WriteLine("Enter another number:");
+        string stringNumber2 = Console.ReadLine();  
+        Console.WriteLine("Enter a third number:");
+        string stringNumber3 = Console.ReadLine();  
+        tri.Side1 = int.Parse(stringNumber1);  
+        tri.Side2 = int.Parse(stringNumber2);  
+        tri.SetSide3(int.Parse(stringNumber3)); 
+        ConfirmOrEditTriangle(tri);
+      }
+    }
+
+    static void CheckTriangleType(Triangle tri)
+    {
+      string result = tri.CheckType();
+      Console.WriteLine("--------------------------------");
+      Console.WriteLine("Your result is: " + result + ".");
+      Console.WriteLine("--------------------------------");
+      Console.WriteLine("What's next?");
+      Console.WriteLine("Would you like to check a new shape (new)?");
+      Console.WriteLine("Please enter 'new' to check the type of a new shape. To exit, enter any key.");
+      string userResponse = Console.ReadLine(); 
+      if (userResponse == "new" || userResponse == "New")
+      {
+        Main();
+      }
+      else
+      {
+        Console.WriteLine("Goodbye!");
+      }
     }
   }
 }
