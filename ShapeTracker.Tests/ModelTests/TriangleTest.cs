3609d3423f38df0dc3994be75477bf75daa5f014
Jacob Maier <jacobamaier@gmail.com>
2023-02-20 13:43:00 -0800

    Test #12: Pass

--- ShapeTracker.Tests/ModelTests/TriangleTest.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTest.cs
@@ -1,103 +1,119 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ShapeTracker.Models;
+using System.Collections.Generic;
+using System;
 
 namespace ShapeTracker.Tests
 {
   [TestClass]
-  public class TriangleTests
+  public class TriangleTests : IDisposable
   {
-
-    [TestMethod]
-    public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
+    public void Dispose()
     {
-      Triangle newTriangle = new Triangle(2, 3, 8);
-      Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
+      Triangle.ClearAll();
     }
+      [TestMethod]
+      public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
+      {
+        Triangle newTriangle = new Triangle(2, 3, 8);
+        Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
+      }
 
-    [TestMethod]
-    public void GetSide1_ReturnsSide1_Int()
-    {
-      int lenght1 = 3;
-      Triangle newTriangle = new Triangle(lenght1, 2, 8);
-      int result = newTriangle.Side1;
-      Assert.AreEqual(lenght1, result);
-    }
+      [TestMethod]
+      public void GetSide1_ReturnsSide1_Int()
+      {
+        int lenght1 = 3;
+        Triangle newTriangle = new Triangle(lenght1, 2, 8);
+        int result = newTriangle.Side1;
+        Assert.AreEqual(lenght1, result);
+      }
 
-    [TestMethod]
-    public void SetSide1_SetsValueOfSide1_Void()
-    {
-      Triangle newTriangle = new Triangle(3, 3, 8);
-      int newLength1 = 44;
-      newTriangle.Side1 = newLength1;
-      Assert.AreEqual(newLength1, newTriangle.Side1);
-    }
+      [TestMethod]
+      public void SetSide1_SetsValueOfSide1_Void()
+      {
+        Triangle newTriangle = new Triangle(3, 3, 8);
+        int newLength1 = 44;
+        newTriangle.Side1 = newLength1;
+        Assert.AreEqual(newLength1, newTriangle.Side1);
+      }
 
-    [TestMethod]
-    public void GetSide2_ReturnsSide2_Int()
-    {
-      int length2 = 3;
-      Triangle newTriangle = new Triangle(2, length2, 8);
-      int result = newTriangle.Side2;
-      Assert.AreEqual(length2, result);
-    }
+      [TestMethod]
+      public void GetSide2_ReturnsSide2_Int()
+      {
+        int length2 = 3;
+        Triangle newTriangle = new Triangle(2, length2, 8);
+        int result = newTriangle.Side2;
+        Assert.AreEqual(length2, result);
+      }
 
-    [TestMethod]
-    public void SetSide2_SetsValueOfSide2_void()
-    {
-      Triangle newTriangle = new Triangle(3, 4, 8);
-      int newLength2 = 6;
-      newTriangle.Side2 = newLength2;
-      Assert.AreEqual(newLength2, newTriangle.Side2);
-    }
+      [TestMethod]
+      public void SetSide2_SetsValueOfSide2_void()
+      {
+        Triangle newTriangle = new Triangle(3, 4, 8);
+        int newLength2 = 6;
+        newTriangle.Side2 = newLength2;
+        Assert.AreEqual(newLength2, newTriangle.Side2);
+      }
 
-    [TestMethod]
-    public void GetSide3_ReturnsSide3_int()
-    {
-      int length3 = 55;
-      Triangle newTriangle = new Triangle(2, 3, length3);
-      int result = newTriangle.GetSide3();
-      Assert.AreEqual(length3, result);
-    }
+      [TestMethod]
+      public void GetSide3_ReturnsSide3_int()
+      {
+        int length3 = 55;
+        Triangle newTriangle = new Triangle(2, 3, length3);
+        int result = newTriangle.GetSide3();
+        Assert.AreEqual(length3, result);
+      }
 
-    [TestMethod]
-    public void SetSide3_SetsValueOfSide3_void()
-    {
-      Triangle newTriangle = new Triangle(3, 4, 8);
-      int newLength3 = 6;
-      newTriangle.SetSide3(newLength3);
-      Assert.AreEqual(newLength3, newTriangle.GetSide3());
-    }
+      [TestMethod]
+      public void SetSide3_SetsValueOfSide3_void()
+      {
+        Triangle newTriangle = new Triangle(3, 4, 8);
+        int newLength3 = 6;
+        newTriangle.SetSide3(newLength3);
+        Assert.AreEqual(newLength3, newTriangle.GetSide3());
+      }
 
-    [TestMethod]
-    public void CheckType_DeterminesWhenNotATriangle_String()
-    {
-      Triangle notATriangle = new Triangle(3, 4, 88);
-      string triType = notATriangle.CheckType();
-      Assert.AreEqual("Not a triangle", triType);
-    }
+      [TestMethod]
+      public void CheckType_DeterminesWhenNotATriangle_String()
+      {
+        Triangle notATriangle = new Triangle(3, 4, 88);
+        string triType = notATriangle.CheckType();
+        Assert.AreEqual("Not a triangle", triType);
+      }
 
-    [TestMethod]
-    public void CheckType_DeterminesWhenScalene_String()
-    {
-      Triangle scaleneTri = new Triangle(3, 4, 5);
-      string triType = scaleneTri.CheckType();
-      Assert.AreEqual("Scalene triangle", triType);
-    }
+      [TestMethod]
+      public void CheckType_DeterminesWhenScalene_String()
+      {
+        Triangle scaleneTri = new Triangle(3, 4, 5);
+        string triType = scaleneTri.CheckType();
+        Assert.AreEqual("Scalene triangle", triType);
+      }
 
-        [TestMethod]
-    public void CheckType_DeterminesWhenIsosceles_String()
-    {
-      Triangle isoscelesTri = new Triangle(5, 4, 4);
-      string triType = isoscelesTri.CheckType();
-      Assert.AreEqual("Isosceles triangle", triType);
-    }
+      [TestMethod]
+      public void CheckType_DeterminesWhenIsosceles_String()
+      {
+        Triangle isoscelesTri = new Triangle(5, 4, 4);
+        string triType = isoscelesTri.CheckType();
+        Assert.AreEqual("Isosceles triangle", triType);
+      }
 
-        [TestMethod]
-    public void CheckType_DeterminesWhenEquilateral_String()
-    {
-      Triangle equiTri = new Triangle(4, 4, 4);
-      string triType = equiTri.CheckType();
-      Assert.AreEqual("Equilateral triangle", triType);
+      [TestMethod]
+      public void CheckType_DeterminesWhenEquilateral_String()
+      {
+        Triangle equiTri = new Triangle(4, 4, 4);
+        string triType = equiTri.CheckType();
+        Assert.AreEqual("Equilateral triangle", triType);
+      }
+
+      [TestMethod]
+      public void GetAll_ReturnsAllTriangleInstances_List()
+      {
+        Triangle tri1 = new Triangle(2, 2, 9);
+        Triangle tri2 = new Triangle(21, 3, 9);
+        Triangle tri3 = new Triangle(1, 3, 9);
+        List<Triangle> expected = new List<Triangle> { tri1, tri2, tri3 };
+        List<Triangle> actualResult = Triangle.GetAll();
+        CollectionAssert.AreEqual(expected, actualResult);
+      }
     }
   }
-}
